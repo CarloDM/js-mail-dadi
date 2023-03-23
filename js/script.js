255b88c63928ac197c525191629de50be939528b
carloDM <carlo.de.mauro.work@gmail.com>
2023-03-23 16:22:40 +0100

    2 variabili

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* 
+1. variabile mail
+2. array lista consentiti
+3. risposta vero o falso
+*/
+
+let eMail = prompt('la tua mail giusta può esser soltanto "pinko,pallon e io"')
+let eMailCon = ['pinko', 'pallon','io']
+
+
+console.log('ciao creo una variabile email', eMail);
+console.log('ciao creo un array mailconsentite', eMailCon);
+
+// 
